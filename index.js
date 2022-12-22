c3da1324834095f06d9a1d60297fad7db6f16275
MD.AL-AMIN <mdalmain67378@gmail.com>
2022-12-22 19:44:39 +0600

    change header and sidebar

--- index.js
+++ index.js
@@ -22,3 +22,31 @@ document.querySelector(".jsFilter").addEventListener("click", function () {
   modeSwitch.addEventListener('click', function () {                      document.documentElement.classList.toggle('light');
    modeSwitch.classList.toggle('active');
   });
+
+/* Sidebar start here */   
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+let navList = document.querySelector(".nav-list");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  navList.classList.toggle("scroll");
+  menuBtnChange();//calling the function(optional)
+});
+
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search icon
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the icons class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the icons class
+ }
+}
+
+/* Sidebar end here */   
